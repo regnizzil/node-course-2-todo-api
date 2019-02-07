df46fb799c6c5b947116692fb4029a7e5ce6ab0c
regnizzil <regnizzil@gmail.com>
2019-02-07 11:40:39 -0500

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 // testing lifecycle method
 beforeEach((done) => {
     // clears database
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 
@@ -31,7 +39,7 @@ describe('POST /todos', () => {
                 }
 
                 // call .find with no arguments fetches everything in the collection
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -52,8 +60,20 @@ it('should not create todo with invalid body data', (done) => {
              }
 
              Todo.find().then((todos) => {
-                 expect(todos.length).toBe(0);
+                 expect(todos.length).toBe(2);
                  done();
              }).catch((e) => done(e));
         });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
